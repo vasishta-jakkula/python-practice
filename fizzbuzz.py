76c71fc6944336c6e35138ae5b86b36401d4cad4
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-02-11 07:42:13 -0600

    unnesessary modules

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for count in range(1,16):
-    if count % 3 == 0 and count % 5 == 0:
-        print("fizzbuzz")
-    elif count % 3 == 0:
-        print("fizz")
-    elif count % 5 == 0:
-        print("buzz")
-    else:
-        print(count)
