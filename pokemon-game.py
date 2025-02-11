ef1e1a2ef6e8278808734f4d1388d29334e80045
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-02-11 17:29:49 -0600

    Update pokemon-game.py

--- pokemon-game.py
+++ pokemon-game.py
@@ -1,1 +1,13 @@
-input_start = input("welcome")
+while True:
+    input_name = input("Hello player what would you like your name to be?\n")
+    input_name_continue =  input(f"your name is {input_name} are you sure?(type y/n)\n")
+    if input_name_continue == "y":
+        choosing()
+        break
+    elif input_name_continue == "n":
+        print('retrying now')
+    else:
+        print("invalid")
+
+def choosing():
+    print("hello!")
