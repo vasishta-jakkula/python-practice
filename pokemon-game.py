719dbaef5584582bf25cdda7eb02519de375d9a1
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-02-11 17:39:49 -0600

    Update pokemon-game.py

--- pokemon-game.py
+++ pokemon-game.py
@@ -1,3 +1,11 @@
+def choosing():
+    input_pick = input("pick a thing to do,(type number)\n1. go to pokedex to pick pokemon\n2. check your rank\n3. fight/play\n")
+    if input_pick == "1":
+        print("h")
+    elif input_pick == "2":
+        print("h")
+    elif input_pick == "3":
+        print("h")
 while True:
     input_name = input("Hello player what would you like your name to be?\n")
     input_name_continue =  input(f"your name is {input_name} are you sure?(type y/n)\n")
@@ -9,5 +17,5 @@ while True:
     else:
         print("invalid")
 
-def choosing():
-    print("hello!")
+pikachu = {"type":"electric","damage":7,"health":30}
+eevee = {"type":"normal","damage":7,"health":30}
