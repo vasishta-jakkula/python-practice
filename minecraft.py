bf9e347ebb192175164234ae583fba5602605213
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-02-22 17:14:13 -0600

    h

--- minecraft.py
+++ minecraft.py
@@ -1,3 +1,4 @@
+from random import *
 village_made = False
 exploration_level = 0
 village_level = 0
@@ -23,9 +24,9 @@ while True:
 print(f"Your name is {input_name}")
 print("you have a wooden sword, and wooden pick in you inventory.\n")
 input1 = input("would you like to make an village here if so type 1\n If you want to gather materials type 2\n if you want to check inventory type 3\n if you quit type 4\n")
-while True:
+
     #village make
-    if input1 == "1":
+if input1 == "1":
         input_village_name = input("starting settlement, what will you call it?")
         input_village_name_check = input_village_name
         if len(input_village_name_check) == 0:
@@ -35,25 +36,41 @@ while True:
             village_made = True
             village_level = village_level + 1
 
-        
-    if input1 == "2":
+random_resources = ""        
+if input1 == "2":
         print("you decided to gather materials")
         if exploration_level == 0:
-            print("you can only gather wood,stone, and food")
+            print("you can only gather wood, and stone")
+            random_resources = random.randint(1,5)
+            if random_resources == 1:
+                wood = wood + 4
+                stone = stone +6
+            if random_resources == 2:
+                wood = wood + 3
+                stone = stone + 4
+            if random_resources == 3:
+                wood = wood + 2
+                stone = stone + 8
+            if random_resources == 4:
+                wood = wood + 5
+                stone = stone +3
+            if random_resources == 5:
+                wood = wood + 5
+                stone = stone + 5
         if exploration_level == 1:
-            print("you can only gather wood,stone,food, and gold")
+            print("you can only gather wood,stone,and gold")
         if exploration_level == 2:
-            print("you can only gather wood,stone,food,gold, and emeralds")
+            print("you can only gather wood,stone,gold, and emeralds")
         if exploration_level == 3:
-            print("you can only gather wood,stone,food,gold,emeralds, and diamonds")
+            print("you can only gather wood,stone,gold,emeralds, and diamonds")
         
         
 
 
-    if input1 == "3":
+if input1 == "3":
         print("h")
 
 
-    if input1 == "4":
+if input1 == "4":
         print("we will now quit the program")
-        # break a while loop 
+        quit()
