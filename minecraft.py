3fb30199bcaa0cccb38a504fbc594a95f75a9749
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-02-22 14:05:43 -0600

    hui

--- minecraft.py
+++ minecraft.py
@@ -23,26 +23,37 @@ while True:
 print(f"Your name is {input_name}")
 print("you have a wooden sword, and wooden pick in you inventory.\n")
 input1 = input("would you like to make an village here if so type 1\n If you want to gather materials type 2\n if you want to check inventory type 3\n if you quit type 4\n")
+while True:
+    #village make
+    if input1 == "1":
+        input_village_name = input("starting settlement, what will you call it?")
+        input_village_name_check = input_village_name
+        if len(input_village_name_check) == 0:
+            print("you village has no letters, try again.")
+        else:
+            print(f"you created a new village,{input_village_name}")
+            village_made = True
+            village_level = village_level + 1
 
-if input1 == "1":
-    input_village_name = input("starting settlement, what will you call it?")
-    input_village_name_check = input_village_name
-    if len(input_village_name_check) == 0:
-        print("you village has no letters, try again.")
-    else:
-      print(f"you created a new village,{input_village_name}")
-      village_made = True
-      village_level = village_level + 1
-
-    
-if input1 == "2":
-    print("you decided to gather materials")
-
-
-if input1 == "3":
-    print("h")
+        
+    if input1 == "2":
+        print("you decided to gather materials")
+        if exploration_level == 0:
+            print("you can only gather wood,stone, and food")
+        if exploration_level == 1:
+            print("you can only gather wood,stone,food, and gold")
+        if exploration_level == 2:
+            print("you can only gather wood,stone,food,gold, and emeralds")
+        if exploration_level == 3:
+            print("you can only gather wood,stone,food,gold,emeralds, and diamonds")
+        
+        
 
 
-if input1 == "4":
-    print("we will now quit the program")
-    # break a while loop 
+    if input1 == "3":
+        print("h")
+
+
+    if input1 == "4":
+        print("we will now quit the program")
+        # break a while loop 
--- pokemon.py
+++ pokemon.py
@@ -2,7 +2,7 @@ import random
 # pokedex
 pokedex = {
       "pikachu" : {"health": 30, "type": "electric", "damage": 10,"special_effect":"electric ball", "defense":0},
-      "charizard" : {"health":20, "type": "fire", "damage": 20,"special_effect":"fire-spin","defense":0},
+      "charizard" : {"health": 30, "type": "fire", "damage": 9,"special_effect":"fire-spin","defense":0},
       "squirtle" : {"health": 25,"type":"water","damage": 11,"special_effect":"water-gush","defense":0},
       "eevee" : {"health": 30, "type":"special", "damage": 11, "special_effect":"peek-a-boo!","defense":0},
 }
@@ -17,16 +17,19 @@ if user_pokemon_name in pokedex:
       print(f"stats of {user_pokemon_name} are {pokemon_stats}")
 else:
         print("invalid registry of pokemon")
-
+    
+user_pokemon_value = pokedex[user_pokemon_name]
+opponent_pokemon_name = ""
+opponent_pokemon_value = {}
 if user_pokemon_name in pokedex:
     input_continue = input("say yes if you would like to continue\n")
     if input_continue == "yes" or input_continue == "Yes":
-        opponent_pokemon_name = ""
         while True:
             random_pokemon_index = random.randint(0, len(pokedex)-1)
             opponent_pokemon_name = list(pokedex.keys())[random_pokemon_index]
             if opponent_pokemon_name != user_pokemon_name:
                   break
+
         print(f"got your opponent is {opponent_pokemon_name}\n")
     elif input_continue == "no":
         print("you said no. So I stop")
@@ -34,26 +37,30 @@ if user_pokemon_name in pokedex:
     else:
         print("invalid try again")
 
-input_battle_1 = input(f"you chose {user_pokemon_name} as your pokemon in battle, what would you like to do?(type the number)\n 1. attack\n 2. Defense\n 3. Lucky shot(has a chance to do special effect)\n")
-while True:
-    #player battle
-        
-    #damage mechanic
+    input_battle_1 = input(f"you chose {user_pokemon_name} as your pokemon in battle, what would you like to do?(type the number)\n 1. attack\n 2. Defense\n 3. Lucky shot(has a chance to do special effect)\n")
+        #player battle
+            
+        #damage mechanic
+
     if input_battle_1 == "1":
-        print(f"you attacked {opponent_pokemon_name}")
 
-    #defense mechanics
+            print(f"you attacked {opponent_pokemon_name}")
+            opponent_pokemon_value = pokedex[opponent_pokemon_name]
+            opponent_pokemon_value["health"] = opponent_pokemon_value["health"] - user_pokemon_value["damage"]
+            print(f"{opponent_pokemon_name} will now go.")
+            
+        
+        #defense mechanics
     elif input_battle_1 == "2":
-        user_pokemon_value = pokedex[user_pokemon_name]
-        print(f"you defended your pokemon,{user_pokemon_value}.")
-        user_pokemon_value["defense"] = user_pokemon_value["defense"] + 1
-        print(f"your defense level is {user_pokemon_value['defense']}")
-        print(f"{opponent_pokemon_name}will now go.")
+            print(f"you defended your pokemon,{user_pokemon_value}.")
+            user_pokemon_value["defense"] = user_pokemon_value["defense"] + 1
+            print(f"your defense level is {user_pokemon_value['defense']}")
+            print(f"{opponent_pokemon_name} will now go.")
 
-    #special move mechanics
+        #special move mechanics
     elif input_battle_1 == "3":
-        print("#SPECIAL MOVE CODE NEEDED")
-    # when battle is won or lost, print message
-    #explore for a account type of approach 
+            print("#SPECIAL MOVE CODE NEEDED")
+        # when battle is won or lost, print message
+        #explore for a account type of approach 
     else:
-         print("invalid response")
+            print("invalid response")
