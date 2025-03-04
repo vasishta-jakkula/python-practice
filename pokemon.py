350e5043531adcd3bc5826eed155919eb9b7171d
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-03-04 07:15:33 -0600

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -2,35 +2,21 @@ import random
 # pokedex
 shop_pokemon = ""
 money = 10
+inventory = {
+      "health_charm" : 1,
+      "strength_charm" : 0
+}
 pokedex = {
       "pikachu" : {"health": 30, "type": "electric", "damage": 10,"defense":0},
       "charizard" : {"health": 30, "type": "fire", "damage": 9,"defense":0},
       "squirtle" : {"health": 25,"type":"water","damage": 11,"defense":0},
       "eevee" : {"health": 30, "type":"special", "damage": 11,"defense":0},
 }
-inventory = {
-      "health_charm" : 1,
-      "strength_charm" : 0
-}
 for key in pokedex.keys():
     print(f"{key}:{pokedex[key]}\n")
-#add weaknesses and special moves later
+
+
 start_input = input("(type num) what would you like to do 1. fight 2. buy pokemon\n")
-
-#shop
-if start_input == "2":
-       print(f"hello welcome to the shop, you may buy pokemon here\n")
-       shop_pokemon == {
-              "bulbasaur" : {"health": 30, "type": "grass","damage": 12,"defense":0,"price": 25},
-              "scorbunny" : {"health": 30, "type":"fire","damage": 12,"defense":0, "price" : 35},
-              "lobunny"  : {"health": 40,"type":"earth","damage":15,"defense":0,"price": 40},
-              "taillow" : {"health": 27,"type":"flying","damage":8,"defense":0,"price": 15},
-              "litten" : {"health": 35,"type":"fire","damage" : 13,"defense":0,"price": 20},
-              }
-       for shop_keys in shop_pokemon.keys():
-            print(f"{shop_keys}:{shop_pokemon[key]}\n")
-
-
 
 #battle
 if start_input == "1":
@@ -72,21 +58,32 @@ if start_input == "1":
                   #damage mechanic
             if input_battle_1 == "1":
 
-                        print(f"you attacked {opponent_pokemon_name}")
+                        print(f"you attacked {opponent_pokemon_name}\n")
                         opponent_pokemon_value = pokedex[opponent_pokemon_name]
-                        opponent_pokemon_value["health"] = opponent_pokemon_value["health"] - user_pokemon_value["damage"]
+                        if opponent_pokemon_value['defense'] == 4 or opponent_pokemon_value["defense"] > 4:
+                              print(f"you attacked {opponent_pokemon_name} with max defense, you did no damage, resseting {opponent_pokemon_name}'s defense")
+                              opponent_pokemon_value["defense"] == 0
+                        else:
+                              opponent_pokemon_value["health"] = opponent_pokemon_value["health"] - user_pokemon_value["damage"]
                         if opponent_pokemon_value["health"] == 0 or opponent_pokemon_value["health"] < 0:
                               print("you won, congrats")
                               break
-                        print(f"your opponent,{opponent_pokemon_name}, has {opponent_pokemon_value['health']} left \n")
+                        print(f"your opponent,{opponent_pokemon_name}, has {opponent_pokemon_value['health']} health left \n")
                         print(f"{opponent_pokemon_name} will now go.\n")
                         opponent_attack = random.randint(1,2)
                         if opponent_attack == 1:
                               opponent_pokemon_value["defense"] = opponent_pokemon_value["defense"] + 1
-                              print(f"your opponent has {opponent_pokemon_value['defense']} defense level now")
+                              if opponent_pokemon_value["defense"] == 4 or opponent_pokemon_value['defense'] > 4:
+                                    opponent_pokemon_value == 4
+                              else:
+                                    print(f"your opponent has {opponent_pokemon_value['defense']} defense level now")
                         elif opponent_attack == 2:
-                              user_pokemon_value['health'] = user_pokemon_value["health"] - opponent_pokemon_value["damage"]
-                              print(f"{opponent_pokemon_name} has attacked you have now {user_pokemon_value['health']} health left\n")
+                              if user_pokemon_value['defense'] == 4 or user_pokemon_value['defense'] > 4:
+                                    print(f"You have maximum defense points, {opponent_pokemon_name} did no damage to you")
+                                    user_pokemon_value['defense'] == 0
+                              else:
+                                    user_pokemon_value['health'] = user_pokemon_value["health"] - opponent_pokemon_value["damage"]
+                                    print(f"{opponent_pokemon_name} has attacked you have now {user_pokemon_value['health']} health left\n")
                               if user_pokemon_value["health"] == 0 or user_pokemon_value["health"] < 0:
                                      print("you have lost")
                                      break
@@ -94,19 +91,47 @@ if start_input == "1":
                   
                   #defense mechanics
             elif input_battle_1 == "2":
-                        print(f"you defended your pokemon,{user_pokemon_value}.")
+                        print(f"you defended your pokemon,{user_pokemon_name}.")
                         user_pokemon_value["defense"] = user_pokemon_value["defense"] + 1
-                        print(f"your defense level is {user_pokemon_value['defense']}\n")
+                        if user_pokemon_value["defense"] == 4 or user_pokemon_value["defense"] > 4:
+                              print("you cannot increase defense, remember next time!")
+                              user_pokemon_value["defense"] == 4
+                        else:
+                              print(f"your defense level is {user_pokemon_value['defense']}\n")
                         print(f"{opponent_pokemon_name} will now go.\n")
                         opponent_attack = random.randint(1,3)
                         if opponent_attack == 1:
                               opponent_pokemon_value["defense"] = opponent_pokemon_value["defense"] + 1
-                              print(f"your opponent has {opponent_pokemon_value['defense']} defense level now")
+                              if opponent_pokemon_value["defense"] == 4 or opponent_pokemon_value['defense'] > 4:
+                                    opponent_pokemon_value == 4
+                              else:
+                                    print(f"your opponent has {opponent_pokemon_value['defense']} defense level now")
                         elif opponent_attack == 2:
-                              user_pokemon_value['health'] = opponent_pokemon_value["damage"]- user_pokemon_value['health']
-                              print(f"your {opponent_pokemon_name} has attacked you you have now {user_pokemon_value['health']}")
+                              if user_pokemon_value['defense'] == 4 or user_pokemon_value['defense'] > 4:
+                                    print(f"You have maximum defense points, {opponent_pokemon_name} did no damage to you")
+                                    user_pokemon_value['defense'] == 0
+                              else:
+                                    user_pokemon_value['health'] = opponent_pokemon_value["damage"]- user_pokemon_value['health']
+                                    print(f"your {opponent_pokemon_name} has attacked you you have now {user_pokemon_value['health']}")
                               if user_pokemon_value["health"] or user_pokemon_value["health"] < 0== 0:
                                      print("you have lost")
                                      break 
                               else:
                                     print("you may go now")
+
+
+#shop
+if start_input == "2":
+       print(f"hello welcome to the shop, you may buy pokemon here\n")
+       shop_pokemon == {
+              "bulbasaur" : {"health": 30, "type": "grass","damage": 12,"defense":0,"price": 25},
+              "scorbunny" : {"health": 30, "type":"fire","damage": 12,"defense":0, "price" : 35},
+              "lobunny"  : {"health": 40,"type":"earth","damage":15,"defense":0,"price": 40},
+              "taillow" : {"health": 27,"type":"flying","damage":8,"defense":0,"price": 15},
+              "litten" : {"health": 35,"type":"fire","damage" : 13,"defense":0,"price": 20},
+              }
+       for shop_keys in shop_pokemon.keys():
+            print(f"{shop_keys}:{shop_pokemon[key]}\n")
+
+
+
