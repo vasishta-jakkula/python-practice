04b01af71a7e82e10443475f07500759c0ba5be1
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-07-13 12:22:12 -0500

    h

--- lifeseries python/lifeseriesThirdLife.py
+++ lifeseries python/lifeseriesThirdLife.py
@@ -1,1 +1,30 @@
-print("hi")
+player = ""
+print('Welcome to Third Life\n')
+input_player = input("(input the num) 1. Grian, 2.Scar, 3.Jimmy, 4. Cleo, 5. Bdouble, 6. BigB,7.Etho, 8. Mumbo, 9.Gem\n")
+if input_player == "1":
+    player = "Grian"
+    print(f"You are {player}\n")
+if input_player == "2":
+    player = "Scar"
+    print(f"You are {player}\n")
+if input_player == "3":
+    player = "Jimmy"
+    print(f"You are {player}\n")
+if input_player == "4":
+    player = "Cleo"
+    print(f"You are {player}\n")
+if input_player == "5":
+    player = "Bdouble"
+    print(f"You are {player}\n")
+if input_player == "6":
+    player ="BigB"
+    print(f"You are {player}\n")
+if input_player == "7":
+    player = "Etho"
+    print(f"You are {player}\n")
+if input_player == "8":
+    player = "Mumbo"
+    print(f"You are {player}\n")
+if input_player == "9":
+    player = "Gem"    
+    print(f"You are {player}\n")
