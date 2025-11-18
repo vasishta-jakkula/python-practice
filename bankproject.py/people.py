267c4d9e7086f4e234c48d867a3bf89cba83ee95
vasishta-jakkula <jakkula.vasishta@gmail.com>
2025-11-17 18:54:19 -0600

    Update people.py

---
+++ bankproject.py/people.py
@@ -0,0 +1,8 @@
+Jimmy = {"age":23,"money":105000,"credit_score": 760,"bday":"august"}
+Sarah = {"age":32,"money":603000,"credit_score": 760, "bday": "july"}
+Thomas = {"age":25,"money":403000,"credit_score": 760, "bday": "february"}
+Teresa = {"age":30,"money":603000,"credit_score": 760, "bday": "december"}
+Garrett = {"age":41,"money":521000,"credit_score": 760, "bday": "November"}
+
+
+
